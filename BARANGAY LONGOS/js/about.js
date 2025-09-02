d0c37b00852fe5b22683f5251e62a648588c2c69
Woshieeeee <anndelle123@gmail.com>
2025-09-03 05:31:51 +0800

    Delete BARANGAY LONGOS directory

--- BARANGAY LONGOS/js/about.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menuToggle = document.querySelector('.menu-link');
-  const navMenu = document.getElementById('menu');
-
-  menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-  });
-});
