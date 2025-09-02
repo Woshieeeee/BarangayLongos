7c74b041fa5113cbc162b26b78e10f555f3a3b27
Woshieeeee <anndelle123@gmail.com>
2025-09-03 05:21:17 +0800

    Add files via upload

---
+++ BARANGAY LONGOS/js/about.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const menuToggle = document.querySelector('.menu-link');
+  const navMenu = document.getElementById('menu');
+
+  menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+  });
+});
